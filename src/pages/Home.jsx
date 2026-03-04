f23881662e20323e8cfad7d4155f1ed2f7ddcc2c
Zahid Khan <zahidkh323@gmail.com>
2026-03-04 13:22:37 +0530

    Chart value is reactive now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import Sidebar from '../components/layout/sidebar'
 import Navbar from '../components/layout/Navbar'
 import ProductGrid from '../components/product/ProductGrid'
+import { useState } from 'react';
 
 const Home = (elem) => {
+  const [cartValue, setCartValue] = useState(0)
+  const addToCart = () => {
+    setCartValue(prev => prev + 1)
+  }
+
   return (
 
     <div className="app-container">
-      <Sidebar />
+      <Sidebar cartValue={cartValue} />
       <div className="main-content">
         <Navbar />
-        <ProductGrid prod={elem.prod}/>
+        <ProductGrid prod={elem.prod} addToCart={addToCart} />
       </div>
     </div>
   )
